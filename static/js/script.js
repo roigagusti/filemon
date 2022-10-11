a4f6f48cadfedf8753dd42608b227fff91e45b17
roigagusti <gutiroma@hotmail.com>
2022-10-11 08:36:44 +0200

    estructura de Filemon con login

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+/*
+* Title: Rempeflix
+* Author: 011h
+* Template: Netflix
+* Version: 0.1
+* Copyright 2022 011h
+* Url: https://www.rompeflix.ml
+
+
+
+Content table
+--------------
+1. Custom scripts
+2. Video scripts
+
+/* ----- 1. Email ----- */
+function correo(usuario,dominio) {
+	return usuario + "@" + dominio
+}
+
+function enlace_correo(a, b) {
+	document.write("<a href='mailto:" + correo(a,b) + "'>")
+}
+
+/* ----- 2. Video ----- */
